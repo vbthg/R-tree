b6d6792aa1d7e37180d1d092aabb45df5bfe001f
PQQuyen <pqquyen040807@gmail.com>
2026-04-19 06:34:38 +0700

    add content in Validator.h

---
+++ header/Validator.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "R-Tree.h"
+#include <vector>
+
+class Validator
+{
+public:
+    // Ham duyet trau de lam ket qua doi chieu (Reference)
+    static std::vector<Point> bruteForceSearch(const std::vector<Point>& points, const Rect& queryBox);
+
+    // Ham so sanh hai tap hop diem (khong quan tam thu tu)
+    static bool areResultsIdentical(std::vector<Point> res1, std::vector<Point> res2);
+
+    // Thuc hien kiem dinh va bao cao loi neu co
+    static void validate(const std::vector<Point>& points, RTree& tree, const Rect& queryBox);
+};
